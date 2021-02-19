a1b83e67e82fb6a1bfc164a24c3250b6ce25f4d4
Noa Andersson <34545334+nooaandersson@users.noreply.github.com>
2021-02-20 00:05:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-    res.sendFile("index.html")
+    res.sendFile(__dirname + "index.html")
 })
 
 // start the server listening for requests
