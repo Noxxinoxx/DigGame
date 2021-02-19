095768e603d05eb4774fb4854d125bab9f2be3d0
Noa Andersson <34545334+nooaandersson@users.noreply.github.com>
2021-02-20 00:04:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const http = require("http")
 const app = express()
-const server = http.createServer(app)
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-    res.sendfile("index.html")
+    res.sendFile("index.html")
 })
 
-server.listen(3000)
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
